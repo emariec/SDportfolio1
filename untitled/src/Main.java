dc84f02ed7015a901835ed9285574e31ea45bf36
ClaraMcNair <79084200+ClaraMcNair@users.noreply.github.com>
2021-03-14 12:54:07 +0100

    added comments in main

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,20 +1,22 @@
 class Main {
     public static void main(String[] args) {
 
-
+        // Creates new shapes: a circle, a rectangle and a triangle:
         Circle cir1 = new Circle(10,30,30);
-
         Rectangle rect1 = new Rectangle(2,2,4,2,2,4,4,4);
-
-        //trekant første punkt i array er punktet for den rette vinkel i trekanten
+        //It is only possible to create rightangled triangles.
+        //the first point will be the right angled corner
         Triangle tri1 = new Triangle(2,2,2,6,7,2);
 
+        // here the printInfo method prints info of the different shapes
         cir1.printInfo();
         rect1.printInfo();
         tri1.printInfo();
 
+        // checks and prints the distance between the shapes cir1 and tr1
         System.out.println("Here is the Euclidean distance between the objects: " + cir1.getEuclideanDist(tri1));
 
+        // checks and print wether a the given point is inside the shape 
         System.out.println("Is the point [5,2] inside cir1? " + cir1.hasPointInside(5,2));
         System.out.println("Is the point [5,2] inside tri1? " + tri1.hasPointInside(5,2));
 
